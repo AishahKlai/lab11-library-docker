51a022d9bdaa674a4c2c5b6c39a537b6493d3480
Aishah <aishah@example.com>
2026-04-30 10:53:26 +0500

    Add repository layer

---
+++ src/main/java/com/example/demo/repository/BorrowRecordRepository.java
@@ -0,0 +1,5 @@
+package com.example.demo.repository;
+import com.example.demo.entity.BorrowRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface BorrowRecordRepository extends JpaRepository<BorrowRecord,Long> {
+}
